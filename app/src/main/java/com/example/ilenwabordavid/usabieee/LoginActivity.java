a986f3225c3d3093e1f37636c33a51cbb8bc243e
demistry <dave11ilen@gmail.com>
2017-05-30 23:25:06 +0100

    Added Intent

--- app/src/main/java/com/example/ilenwabordavid/usabieee/LoginActivity.java
+++ app/src/main/java/com/example/ilenwabordavid/usabieee/LoginActivity.java
@@ -1,7 +1,11 @@
 package com.example.ilenwabordavid.usabieee;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
+        Button quizButton = (Button) findViewById(R.id.quiz_button);
+        EditText username = (EditText) findViewById(R.id.username);
+        final String userName = username.getText().toString();
+        quizButton.setOnClickListener(
+                new Button.OnClickListener(){
+                    public void onClick(View v){
+                        Intent triviaIntent= new Intent(LoginActivity.this, TriviaActivity.class);
+                        triviaIntent.putExtra("Username", userName);
+                        startActivity(triviaIntent);
+                    }
+                }
+        );
     }
 }
